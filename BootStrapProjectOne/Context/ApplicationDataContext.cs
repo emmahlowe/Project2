c598848a7482532cbf9cb54da215b93f74283e44
Emma <emmahlowe@gmail.com>
2019-11-30 15:56:31 -0700

    It works!!!

--- BootStrapProjectOne/Context/ApplicationDataContext.cs
+++ BootStrapProjectOne/Context/ApplicationDataContext.cs
@@ -16,5 +16,7 @@ namespace BootStrapProjectOne.Context
         public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
 
         public System.Data.Entity.DbSet<BootStrapProjectOne.Models.Student> Students { get; set; }
+
+        public System.Data.Entity.DbSet<BootStrapProjectOne.Models.Answer> Answers { get; set; }
     }
 }
